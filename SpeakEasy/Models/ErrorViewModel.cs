f301695408ca716ca760e535ccde919318c6b5f4
SneyderH <sneyderhl2002@gmail.com>
2024-11-08 21:34:31 -0500

    Agregar archivos de proyecto.

---
+++ SpeakEasy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpeakEasy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
